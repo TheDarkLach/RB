324242fa3fe85ec8fb9c17295712cf54b703746b
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2021-01-10 19:23:40 -0800

    woo

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+/*red black tree insertion by faizan karim*/
 
+#include <cstring>
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <cmath>
+#include "node.h"
+
+using namespace std;
+
+int main()
+{
+    
+}
